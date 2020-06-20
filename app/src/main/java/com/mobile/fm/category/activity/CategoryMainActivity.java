030c153be34402ec9d12e632b449d6fc7b7405fa
Sohyeonbin <hyeonbin3447@naver.com>
2020-06-21 06:12:15 +0900

    카테고리 게시판 수정(Adapterposition 수정필요)

--- app/src/main/java/com/mobile/fm/category/activity/CategoryMainActivity.java
+++ app/src/main/java/com/mobile/fm/category/activity/CategoryMainActivity.java
@@ -1,5 +1,6 @@
 package com.mobile.fm.category.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,31 +14,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.mobile.fm.R;
+import com.mobile.fm.category.adapter.CategoryBoardAdapter;
 import com.mobile.fm.category.adapter.CategoryRecommendationsAdapter;
 import com.mobile.fm.category.data.RecommendationsListItem;
+import com.mobile.fm.exerciseboard.PostInfo;
 import com.mobile.fm.exerciseboard.activity.MainActivity;
-import com.mobile.fm.music.adapter.CustomAdapterForSquareList;
-import com.mobile.fm.music.adapter.CustomAdapterForSquareMusic;
-import com.mobile.fm.music.data.Music;
-import com.mobile.fm.music.data.MusicListSquare;
+import com.mobile.fm.exerciseboard.activity.PostActivity;
+import com.mobile.fm.exerciseboard.adapter.HomeAdapter;
 import com.mobile.fm.music.decoration.ListDecoration;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class CategoryMainActivity  extends AppCompatActivity implements View.OnClickListener {
 
     private String category_name;
     private LinearLayout background;
     private RecyclerView recyclerView;
+    private RecyclerView boardRecyclerView;
     private CategoryRecommendationsAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.LayoutManager BlayoutManager;
     private ArrayList<Long> NumList;
     private ArrayList<RecommendationsListItem> arrayList;
     private FirebaseDatabase database;
@@ -45,9 +55,15 @@ public class CategoryMainActivity  extends AppCompatActivity implements View.OnC
     private Button recommendations_btn;
     private Button board_btn;
     private long[] NumArray;
+    private boolean updating;
+    private FirebaseFirestore firebaseFirestore;
+    private CategoryBoardAdapter categoryBoardAdapter;
+    private ArrayList<PostInfo> postList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_main);
         Intent intent = getIntent();
@@ -61,28 +77,37 @@ public class CategoryMainActivity  extends AppCompatActivity implements View.OnC
         recommendations_btn.setOnClickListener(this);
         board_btn.setOnClickListener(this);
 
+
         database = FirebaseDatabase.getInstance();
         init_today_picks(category_name);
+
     }
 
-    private void init_today_picks(final String category_name){
-        recyclerView =findViewById(R.id.category_main_recyclerview_for_recommendations);
-        recyclerView.setHasFixedSize(true);//리사이클러뷰 기존 성능 강화;
+    private void init_today_picks(final String category_name) {
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        postList = new ArrayList<>();
 
+
+        recyclerView = findViewById(R.id.category_main_recyclerview_for_recommendations);
+        boardRecyclerView = findViewById(R.id.category_main_recyclerview_for_board);
+        recyclerView.setHasFixedSize(true);//리사이클러뷰 기존 성능 강화;
+        boardRecyclerView.setHasFixedSize(true);
         //레이아웃 매니저가 수평으로 배치하게 만들어줌.
         //리니어 매니저는 수평,수직,or 일렬 cf)Gridlayout은 바둑판형으로 표시.
-        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        BlayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
-        NumList =new ArrayList<>();
-        arrayList=new ArrayList<>();
-        databaseReference = database.getReference("Today"+category_name); //오늘의 노래들 번호를 받아오기위해 레퍼런스를 가져옴.
+        boardRecyclerView.setLayoutManager(BlayoutManager);
+        NumList = new ArrayList<>();
+        arrayList = new ArrayList<>();
+        databaseReference = database.getReference("Today" + category_name); //오늘의 노래들 번호를 받아오기위해 레퍼런스를 가져옴.
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //파이어 베이스  db를 받아오는 곳.
                 NumList.clear();  //기존 배열 초기화, 방지차원.
-                for (DataSnapshot snapshot: dataSnapshot.getChildren()){//반복문으로 데이터 list를 추출해냄
-                    Long num =snapshot.getValue(Long.TYPE); // 만들어 뒀떤 Music 객체에  데이터를 담음.
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {//반복문으로 데이터 list를 추출해냄
+                    Long num = snapshot.getValue(Long.TYPE); // 만들어 뒀떤 Music 객체에  데이터를 담음.
                     NumList.add(num);   //담은 데이터를 배열리스트ㅔ 넣고 리사이클 뷰로 보낼 준비
                 }
 //                adapter.notifyDataSetChanged();//   리스트 저장 및  새롴고침
@@ -91,30 +116,31 @@ public class CategoryMainActivity  extends AppCompatActivity implements View.OnC
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 //디비를 가져오던 중 에러 발생시.
-                Log.e(category_name+"Activity", String.valueOf(databaseError.toException()));
+                Log.e(category_name + "Activity", String.valueOf(databaseError.toException()));
             }
         });
         databaseReference = database.getReference(category_name);
 
-        System.out.println(category_name+"까지 db가져오기 성공");
+        System.out.println(category_name + "까지 db가져오기 성공");
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 arrayList.clear();
                 //dataSnapshot 여기에 Music에 대한 데이터들이 전부 들어 있다. 그 데이터들은 DataSnapshot 형식임.
-                NumArray=new long[NumList.size()];
-                int idx=0;
-                for(Long test : NumList){
-                    System.out.println("값"+test);
+                NumArray = new long[NumList.size()];
+                int idx = 0;
+                for (Long test : NumList) {
+                    System.out.println("값" + test);
                     arrayList.add(dataSnapshot.child(Integer.toString(test.intValue())).getValue(RecommendationsListItem.class));
-                    NumArray[idx++]=test;
+                    NumArray[idx++] = test;
                 }
                 adapter.notifyDataSetChanged();//   리스트 저장 및  새롴고침
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 //디비를 가져오던 중 에러 발생시.
-                Log.e(category_name+"Activity", String.valueOf(databaseError.toException()));
+                Log.e(category_name + "Activity", String.valueOf(databaseError.toException()));
             }
         });
         adapter = new CategoryRecommendationsAdapter(arrayList, this);
@@ -124,13 +150,52 @@ public class CategoryMainActivity  extends AppCompatActivity implements View.OnC
             public void onItemClick(View v, int position) {
                 startYouTube(arrayList.get(position).getUrl());
             }
-        }) ;
+        });
         recyclerView.setAdapter(adapter);//리사이클러 뷰의 어댑터 연결
 
         //각 ITEM 마다 갭 설정.
         ListDecoration decoration = new ListDecoration();
         recyclerView.addItemDecoration(decoration);
+
+
+        //게시판 글 정보 받아오기
+        final Boolean clear=true;
+        updating = true;
+        Date date = postList.size() == 0 || clear ? new Date() : postList.get(postList.size() - 1).getCreatedAt();
+        CollectionReference collectionReference = firebaseFirestore.collection("posts");
+        collectionReference.orderBy("createdAt", Query.Direction.DESCENDING).whereLessThan("createdAt", date).limit(10).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (clear) {
+                                postList.clear();
+                            }
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                if (category_name.equals(document.getData().get("boardSelect").toString()))
+                                    postList.add(new PostInfo(
+                                            document.getData().get("title").toString(),
+                                            (ArrayList<String>) document.getData().get("contents"),
+                                            (ArrayList<String>) document.getData().get("formats"),
+                                            document.getData().get("publisher").toString(),
+                                            new Date(document.getDate("createdAt").getTime()),
+                                            document.getId(),
+                                            document.getData().get("nid").toString()));
+                            }
+                            categoryBoardAdapter.notifyDataSetChanged();
+                        } else {
+
+                        }
+                        updating = false;
+                    }
+                });
+        categoryBoardAdapter = new CategoryBoardAdapter(this,postList );
+        boardRecyclerView.setAdapter(categoryBoardAdapter);//게시판 리사이클러 뷰의 어댑터 연결
+
     }
+
+
 
     private void set_background_color(String category_name){
         background=findViewById(R.id.category_bg);
@@ -177,4 +242,5 @@ public class CategoryMainActivity  extends AppCompatActivity implements View.OnC
         }
 
     }
+
 }
---
+++ app/src/main/java/com/mobile/fm/category/adapter/CategoryBoardAdapter.java
@@ -0,0 +1,158 @@
+package com.mobile.fm.category.adapter;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mobile.fm.R;
+import com.mobile.fm.exerciseboard.FirebaseHelper;
+import com.mobile.fm.exerciseboard.PostInfo;
+import com.mobile.fm.exerciseboard.activity.PostActivity;
+import com.mobile.fm.exerciseboard.adapter.HomeAdapter;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+
+public class CategoryBoardAdapter extends RecyclerView.Adapter<CategoryBoardAdapter.CustomViewHolder>{
+    private ArrayList<PostInfo> arrayList;
+    private Context context;
+    private Activity activity;
+    private FirebaseHelper firebaseHelper;
+    // 리스너 객체 참조를 저장하는 변수
+    private CategoryBoardAdapter.OnItemClickListener mListener = null ;
+
+
+
+
+    public CategoryBoardAdapter(Activity activity, ArrayList<PostInfo> arrayList) {
+        this.arrayList = arrayList;
+        this.activity = activity;
+
+        firebaseHelper = new FirebaseHelper(activity);
+    }
+
+    @NonNull
+    @Override
+    //뷰 홀더를 처음 부를때 뷰 홀더를 객체화.
+    public CategoryBoardAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.board_list_item, parent, false);
+        CategoryBoardAdapter.CustomViewHolder holder = new CategoryBoardAdapter.CustomViewHolder(view);
+        //새로 생성된 뷰홀더 객체를 리턴하는데 , 뷰 객체를 담아서 리턴한다.
+        final CategoryBoardAdapter.CustomViewHolder customViewHolder = new CategoryBoardAdapter.CustomViewHolder(view);
+
+        return holder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryBoardAdapter.CustomViewHolder holder, int position) {
+        //만들어진 뷰 홀더에 데이터만 다르게 담음.
+        //glide는 이미지 로딩에 빠름.
+//        Glide.with(holder.itemView)
+//                .load(arrayList.get(position).getCover())
+//                .into(holder.cover);
+        holder.title.setText(arrayList.get(position).getTitle());
+        arrayList.get(position).getFormats().indexOf(0);
+        if(arrayList.get(position).getFormats().size()==0){
+            holder.content.setText("내용없음");
+        }
+        else if (arrayList.get(position).getFormats().get(0).equals("text")) {
+            holder.content.setText(arrayList.get(position).getContents().get(0));
+
+        }
+        else {
+            holder.content.setText("사진, 동영상 첨부");
+        }
+        holder.username.setText(arrayList.get(position).getNid());
+
+        SimpleDateFormat dateFormat= new SimpleDateFormat("yyMMddHHmmss");
+        Date ndate= new Date();
+        Date ydate= arrayList.get(position).getCreatedAt();
+        long diff=ndate.getTime()-ydate.getTime();
+        diff=diff/60000;
+        String time;
+        if(ndate.getDay()!=ydate.getDay()) {
+            time= new SimpleDateFormat("MM.dd", Locale.getDefault()).format(arrayList.get(position).getCreatedAt()).toString();
+        }
+        else if(diff<61){
+            time= diff+"";
+            time+="분 전";
+
+        }else if(diff<3601) {
+            time = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(arrayList.get(position).getCreatedAt()).toString();
+        }
+
+        else{
+            time= new SimpleDateFormat("MM.dd", Locale.getDefault()).format(arrayList.get(position).getCreatedAt()).toString();
+        }
+
+        holder.time.setText(time);
+    }
+
+    @Override
+    public int getItemCount() {
+        return (arrayList!=null ? arrayList.size(): 0);
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View v, int position) ;
+    }
+
+    // OnItemClickListener 리스너 객체 참조를 어댑터에 전달하는 메서드
+    public void setOnItemClickListener(CategoryBoardAdapter.OnItemClickListener listener) {
+       this.mListener = listener ;
+    }
+
+    //뷰를 담아두는 곳.
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+
+//        private String title;
+////        private ArrayList<String> contents;
+////        private ArrayList<String> formats;
+////        private String publisher;//유저 아이디
+////        private Date createdAt;
+////        private String nid;//유저 닉네임
+////        private String id;//문서 이름(파이어베이스 문서 이름)
+////        private String boardSelect;
+////        private int numComments=0;
+        TextView title;
+        TextView content;
+        TextView time;
+        TextView username;
+        TextView commentNumber;
+
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.title = itemView.findViewById(R.id.tv_title);
+            this.content = itemView.findViewById(R.id.tv_content);
+            this.time=itemView.findViewById(R.id.tv_time);
+            this.username=itemView.findViewById(R.id.tv_userName);
+            this.commentNumber=itemView.findViewById(R.id.tv_commentNumber);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos=getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION) {
+                        Intent intent = new Intent(activity, PostActivity.class);
+                        intent.putExtra("postInfo", arrayList.get(pos));
+                        activity.startActivity(intent);
+
+                    }
+                }
+            });
+
+        }
+    }
+}
+
