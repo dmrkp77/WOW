38361ef6326d7a8f98673edf1b38cf6c87f0452d
dmrkp77 <slayers0510@gmail.com>
2020-06-21 17:49:28 +0900

    SplashActivity 추가 (로딩화면)

---
+++ app/src/main/java/com/mobile/fm/login/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.mobile.fm.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.mobile.fm.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
